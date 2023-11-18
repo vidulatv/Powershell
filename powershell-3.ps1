6d746abb536a7e149b4ba726ef9c71e993100453
vidulatv <143807529+vidulatv@users.noreply.github.com>
2023-11-18 10:45:15 +0530

    Add files via upload

---
+++ powershell-3.ps1
@@ -0,0 +1,9 @@
+new-item sample.txt
+# Define the sentence
+$sentence = "This is a sample sentence that will be written to a text file."
+
+# Specify the file path
+$filePath = "D:\vidula\sample.txt"
+
+# Use Out-File to create or overwrite the file and write the sentence
+$sentence | Out-File -FilePath $filePath
