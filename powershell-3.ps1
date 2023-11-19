92ed2c4b64f22a724f49a7d3aeef9a9850b93ded
vidulatv <vidula.radhakrishnan@gmail.com>
2023-11-19 14:32:49 +0530

    script changed

--- powershell-3.ps1
+++ powershell-3.ps1
@@ -4,6 +4,7 @@ $sentence = "This is a sample sentence that will be written to a text file."
 
 # Specify the file path
 $filePath = "D:\vidula\sample.txt"
+#change
 
 # Use Out-File to create or overwrite the file and write the sentence
 $sentence | Out-File -FilePath $filePath
